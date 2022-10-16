651c8d63180ddefe92aa4f06d42a03193be8cff0
Beautus <gumedesduduzo@gmail.com>
2022-10-16 14:20:26 +0200

    fix: hyperlink hosts for desktop

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -213,14 +213,14 @@ export function Layout({ children }) {
         <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
           <span className="font-mono text-slate-500">Hosted by</span>
           <span className="mt-6 flex gap-6 font-bold text-slate-900">
-            {hosts.map((host, hostIndex) => (
-              <Fragment key={host}>
+            {hosts.map(([name, href], hostIndex) => (
+              <Fragment key={name}>
                 {hostIndex !== 0 && (
                   <span aria-hidden="true" className="text-slate-400">
                     /
                   </span>
                 )}
-                {host}
+                <Link href={href}>{name}</Link>
               </Fragment>
             ))}
           </span>
